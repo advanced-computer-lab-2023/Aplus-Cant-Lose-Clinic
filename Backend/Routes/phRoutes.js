843ef850ec3a4f5c6193790c9b0df758592efaf2
Sohailaaa <98600518+Sohailaaa@users.noreply.github.com>
2023-10-07 11:54:13 +0300

    removed

--- Backend/Routes/phRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router(); // Create an instance of the Express router
-
-const {addPharmacist} = require("../controllers/phController");
-router.post("/addPharmacist", addPharmacist); 
-
-module.exports=router;
