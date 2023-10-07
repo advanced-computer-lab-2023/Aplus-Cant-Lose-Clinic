f10054955085c7581e393b40a1e733936d5c209a
Shark23p4 <yelsharkawy42@gmail.com>
2023-10-08 00:26:59 +0300

    Update paRoutes.js

--- Backend/Routes/paRoutes.js
+++ Backend/Routes/paRoutes.js
@@ -5,10 +5,6 @@ const router = express.Router(); // Create an instance of the Express router
 const {addPatient} = require("../controllers/paController");
 router.post("/addPatient", addPatient); 
 
-const {viewMedicine, searchMedicineByName, filterMedicineByUse} = require("../controllers/userController");
-router.get("/viewMedicine", viewMedicine);
-router.get("/searchMedicineByName", searchMedicineByName);
-router.get("/filterMedicineByUse", filterMedicineByUse);
 
 module.exports=router;
 
