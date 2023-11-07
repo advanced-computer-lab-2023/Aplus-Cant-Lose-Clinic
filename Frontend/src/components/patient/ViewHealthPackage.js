b6120ca476823a25e62f8ee168a569ed8a1306ad
MohamedFayzalla <126291509+MohamedFayzalla@users.noreply.github.com>
2023-11-07 03:24:38 +0200

    make some update in frontend

--- Frontend/src/components/patient/ViewHealthPackage.js
+++ Frontend/src/components/patient/ViewHealthPackage.js
@@ -1,4 +1,5 @@
-import React from 'react';
+
+import React, { useState, useContext } from 'react';
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,63 +8,35 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { useSelector, useDispatch } from "react-redux";
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { viewHealthP, deleteHpackages, editPackFront, updatePack } from '../../features/adminSlice';
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
 import { Button, Typography } from "@mui/material";
-import { NavLink } from 'react-router-dom';
-import { useContext } from "react";
 import { SnackbarContext } from "../../App";
-import Dialog from "@mui/material/Dialog";
 
 export default function Hpackages() {
   const snackbarMessage = useContext(SnackbarContext);
-
   const [editRow, setEditRow] = useState({});
   const [id, setId] = useState(-1);
   const [idx, setIdx] = useState(-1);
+  const [subscribed, setSubscribed] = useState({}); // Track subscription status
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(viewHealthP())
+    dispatch(viewHealthP());
   }, [dispatch]);
 
   const dummyData = useSelector((state) => state.admin.hpackages);
+
   const handleUnSubscribe = (id) => {
-    // dispatch(deleteHpackages(id));
+    // Dispatch your unsubscribe logic here
+    // Update the subscribed state accordingly
+    setSubscribed({ ...subscribed, [id]: false });
   };
 
-  
-  const handleSubscribe = (event) => {
-  //   event.preventDefault();
-
-  //   const sampleData = {
-  //     type: event.target.elements.type.value,
-  //     rate: event.target.elements.rate.value,
-  //     doctorDisc: event.target.elements.doctorDisc.value,
-  //     medicineDisc: event.target.elements.medicineDisc.value,
-  //     familyDisc: event.target.elements.familyDisc.value,
-    
-  //   };
-
-  //   console.log(sampleData);
-  //   dispatch(editPackFront({ idx: idx, newData: sampleData }));
-    
-  //  const response = dispatch(updatePack({ id: id, newData: sampleData }));
-
-   
-
-  //   response.then((responseData) => {
-  //     console.log(responseData);
-  //     if (responseData.payload === undefined) {
-  //       snackbarMessage(`error: ${responseData} has occurred`, "error");
-  //     } else {
-  //       snackbarMessage("You have successfully edited", "success");
-       
-  //     }
-  //   });
-  
+  const handleSubscribe = (row, index) => {
+    // Implement your subscribe logic here
+    // After subscribing, update the subscribed state
+    setSubscribed({ ...subscribed, [row._id]: true });
   };
 
   const tableStyle = {
@@ -77,9 +50,23 @@ export default function Hpackages() {
   const cellStyle = {
     fontSize: "14px",
   };
+
+  const subscribeButtonStyle = {
+    backgroundColor: '#004E98',
+    color: 'white',
+    marginRight: '20%',
+    width: '70%', // Set the width to the desired value for both buttons
+  };
+
+  const unsubscribeButtonStyle = {
+    backgroundColor: '#a80b0b', // Red background color for Unsubscribe
+    color: 'white',
+    marginRight: '20%',
+    width: '70%', // Set the width to the desired value for both buttons
+  };
+
   return (
     <>
-     
       <TableContainer component={Paper} style={tableStyle}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -97,12 +84,8 @@ export default function Hpackages() {
               <TableCell align="left" style={cellStyle}>
                 familyDisc
               </TableCell>
-              <TableCell align="left" style={cellStyle}>
-
-              </TableCell>
-              <TableCell align="left" style={cellStyle}>
-
-              </TableCell>
+              <TableCell align="left" style={cellStyle}></TableCell>
+              <TableCell align="left" style={cellStyle}></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -127,28 +110,34 @@ export default function Hpackages() {
                   {row.familyDisc}
                 </TableCell>
                 <TableCell align="left" style={cellStyle}>
-                  <Button sx={{ backgroundColor: '#004E98', color: 'white', marginRight: '20%', width:'90%'}} onClick={() =>  {console.log(row); handleSubscribe(row, index)}}>
-                    <Typography>
-                      Subscribe
-                    </Typography>
-                  </Button>
-                </TableCell>
-                <TableCell align="left" style={cellStyle}>
-                  <Button sx={{ backgroundColor: '#a80b0b', color: 'white', marginRight: '20%' ,width:'70%'}} onClick={() => handleUnSubscribe(row._id)}>
-                    <Typography>
-                    UnSubscribe
-                    </Typography>
-                  </Button>
+                  {!subscribed[row._id] ? (
+                    <Button
+                      sx={subscribeButtonStyle}
+                      onClick={() => {
+                        console.log(row);
+                        handleSubscribe(row, index);
+                      }}
+                    >
+                      <Typography>
+                        Subscribe
+                      </Typography>
+                    </Button>
+                  ) : (
+                    <Button
+                      sx={unsubscribeButtonStyle}
+                      onClick={() => handleUnSubscribe(row._id)}
+                    >
+                      <Typography>
+                        UnSubscribe
+                      </Typography>
+                    </Button>
+                  )}
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-    
     </>
   );
 }
-
-
-
