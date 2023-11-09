2dc262320b3ca53dbaf06c5867a9001b0a3cfa10
youssefbayoumi <youssefbayoumi10@gmail.com>
2023-11-09 21:47:15 +0200

    added stripe library

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ mongoose.set("strictQuery", false);
 require("dotenv").config();
 const connectDB = require("./config/db");
 const cookieParser = require('cookie-parser');
+const stripe=require('stripe');
 
 // App variables
 const app = express(); // Move this line to the top
