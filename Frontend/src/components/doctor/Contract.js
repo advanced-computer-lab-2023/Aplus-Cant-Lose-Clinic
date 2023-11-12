53f57a881e3f1fac1648d6f59e604d5a5fbec4ae
AbdelrahmanAmr20 <bido.amr2001@gmail.com>
2023-11-12 06:45:14 +0200

    Update for Accepting contract
    Now it accepts and the function works but still want to modify when i press accept to return back home for the doctor and when pressing view downloads the contract pdf

--- Frontend/src/components/doctor/Contract.js
+++ Frontend/src/components/doctor/Contract.js
@@ -13,79 +13,85 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useEffect } from 'react';
 import { API_URL } from "../../Consts.js";
-export default function Contract() {
-    const { doctorId } = useParams();
-    const [Doctor, setDoctor] = useState(null);
+import {NavLink} from 'react-router-dom';
 
-    const SetContracts = async () => {
-        try {
-          const response = await axios.put( `${API_URL}/doctor/acceptContract/${doctorId}`);
+
+
+export default function Contract() {
+  const { doctorId } = useParams();
+  const [doctor, setDoctor] = useState(null);
+ 
+  const SetContracts = async () => {
+      try {
+          const response = await axios.put(`http://localhost:8000/api/doctor/acceptContract/${doctorId}`);
           const ContractsData = response.data.doctor;
           console.log(ContractsData);
-        } catch (error) {
+         
+      } catch (error) {
           console.error("Error fetching Contracts:", error);
-        }
       }
-      const getDoctor = async () => {
-        try {
-          const response = await axios.get( `${API_URL}/doctor/getDoctor/${doctorId}`);
+  }
+
+  const getDoctor = async () => {
+      try {
+          const response = await axios.get(`http://localhost:8000/api/doctor/getDoctor/${doctorId}`);
           const getDoctorData = response.data.doctor;
           setDoctor(getDoctorData);
-        } catch (error) {
-          console.error("Error fetching Contracts:", error);
-        }
+      } catch (error) {
+          console.error("Error fetching Doctor:", error);
       }
-      useEffect(() => {
-        getDoctor();
-        console.log(Doctor);
-      }, []); 
-       const tableStyle = {
-    width: "80%",
-    marginLeft: "50px",
-    boxShadow: "5px 5px 5px 5px #8585854a",
-    marginTop: "30px",
-    marginBottom: "20px",
+  }
+
+  useEffect(() => {
+      getDoctor();
+  }, []);
+
+  const tableStyle = {
+      width: "80%",
+      marginLeft: "50px",
+      boxShadow: "5px 5px 5px 5px #8585854a",
+      marginTop: "30px",
+      marginBottom: "20px",
   };
 
   const cellStyle = {
-    fontSize: "14px",
+      fontSize: "14px",
   };
 
   return (
-    <TableContainer component={Paper} style={tableStyle}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell style={cellStyle}>Job Name</TableCell>
-            <TableCell align="left" style={cellStyle}>Accept</TableCell>
-            <TableCell align="left" style={cellStyle}>View</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-            {/* {Doctor.contract==='pending'?(<TableRow
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                Doctor
-              </TableCell>
-              <TableCell>
-                <Button sx={{ backgroundColor: '#0007a5ab', color: 'white', width: '35%' }} onClick={()=>SetContracts} >
-                  <Typography>
-                    Accept
-                  </Typography>
-                </Button>
-              </TableCell>
-              <TableCell>
-                <Button sx={{ backgroundColor: '#0007a5ab', color: 'white', width: '35%' }}  >
-                  <Typography>
-                    View
-                  </Typography>
-                </Button>
-              </TableCell>
-            </TableRow>):('')}
-            */}
-        </TableBody>
-      </Table>
-    </TableContainer>
+      <TableContainer component={Paper} style={tableStyle}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                  <TableRow>
+                      <TableCell style={cellStyle}>Job Name</TableCell>
+                      <TableCell align="left" style={cellStyle}>Accept</TableCell>
+                      <TableCell align="left" style={cellStyle}>View</TableCell>
+                  </TableRow>
+              </TableHead>
+              <TableBody>
+                  {doctor && doctor.contract && doctor.contract === 'pending' ? (
+                      <TableRow sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                          <TableCell component="th" scope="row">
+                              Doctor
+                          </TableCell>
+                          <TableCell>
+                              <Button sx={{ backgroundColor: '#0007a5ab', color: 'white', width: '35%' }} onClick={SetContracts}>
+                                  <Typography>
+                                      Accept
+                                  </Typography>
+                              </Button> 
+                          </TableCell>
+                          <TableCell>
+                              <Button sx={{ backgroundColor: '#0007a5ab', color: 'white', width: '35%' }}>
+                                  <Typography>
+                                      View
+                                  </Typography>
+                              </Button>
+                          </TableCell>
+                      </TableRow>
+                  ) : null}
+              </TableBody>
+          </Table>
+      </TableContainer>
   );
 }
