4fda6fc00442768cd3b8431a35845d88282f7b79
Ahmed-Elgamel <ahmed_hamed2242@yahoo.com>
2023-12-16 14:04:03 +0200

    route mofified

--- Backend/Routes/paRoutes.js
+++ Backend/Routes/paRoutes.js
@@ -248,7 +248,7 @@ router.get("/viewHealthPackagesPatient/:patientId", viewHealthPackagesPatient);
 router.get("/viewWallet/:patientId", viewWallet);
 router.get("/healthPackageInfo/:patientId/:healthPackageId", healthPackageInfo);
 
-router.post("/createCheckoutSession/:id/:pid", createCheckoutSession);
+router.post("/createCheckoutSession/:id/:h_id", createCheckoutSession);
 router.post(
   "/createAppointmentCheckoutSession/:amount/:appointmentId/:patientId",
   createAppointmentCheckoutSession
