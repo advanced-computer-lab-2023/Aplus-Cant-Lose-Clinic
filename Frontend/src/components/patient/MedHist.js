d6c910f4cb87337aea832b91cb49e70115f353ad
Sohailaaa <98600518+Sohailaaa@users.noreply.github.com>
2023-12-18 02:19:39 +0200

    Update MedHist.js

--- Frontend/src/components/patient/MedHist.js
+++ Frontend/src/components/patient/MedHist.js
@@ -1,5 +1,4 @@
 import React, { useState, useRef } from "react";
-import { Form, Row, Col, Button } from "react-bootstrap";
 import Dropzone from "react-dropzone";
 import axios from "axios";
 import { Document, Page } from "react-pdf";
@@ -13,8 +12,6 @@ import { Dialog, DialogTitle, DialogContent, DialogActions } from '@mui/material
 
 import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
-
-import AccountAvatar from "../Authentication/AccountAvatar.js";
 
 const MedHist = ({ open, onClose }) => {
   const { id, role } = useSelector((state) => state.user);
@@ -62,6 +59,8 @@ const MedHist = ({ open, onClose }) => {
   const handleOnSubmit = async (event) => {
     event.preventDefault();
     try {
+      console.log("hi");
+
       const { title, description } = state;
       if (title.trim() !== "" && description.trim() !== "") {
         if (file) {
@@ -70,18 +69,16 @@ const MedHist = ({ open, onClose }) => {
           formData.append("title", title);
           formData.append("description", description);
           setErrorMsg("");
-
           await axios.post(`${API_URL}/patient/upload/${id}`, formData, {
             headers: {
               "Content-Type": "multipart/form-data",
             },
           });
-
+          console.log("hi");
           setSnackbarSeverity("success");
           setSnackbarMessage("File uploaded successfully");
           setSnackbarOpen(true);
 
-          navigate("/HealthRecords");
           onClose(); // Close the dialog
         } else {
           setErrorMsg("Please select a file to add.");
@@ -103,39 +100,31 @@ const MedHist = ({ open, onClose }) => {
   return role === "patient" ? (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Add Medical History</DialogTitle>
-      <DialogContent sx={{mt:"20px"}}>
-        <Form
-          onSubmit={handleOnSubmit}
-          style={{ width: "80%", maragin: "40px",paddingLeft: "50px" }}
-        >
+      <DialogContent sx={{ mt: "20px" }}>
+        <form onSubmit={handleOnSubmit} style={{ width: "80%", margin: "40px", paddingLeft: "50px" }}>
           {errorMsg && <p className="errorMsg">{errorMsg}</p>}
-          <Row>
-            <Col>
-              <Form.Group controlId="title">
-                <Form.Control
-                  type="text"
-                  sx={{ width: "50%" }}
-                  name="title"
-                  value={state.title || ""}
-                  placeholder="Enter title"
-                  onChange={handleInputChange}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group controlId="description">
-                <Form.Control
-                  type="text"
-                  name="description"
-                  value={state.description || ""}
-                  placeholder="Enter description"
-                  onChange={handleInputChange}
-                />
-              </Form.Group>
-            </Col>
-          </Row>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              value={state.title || ""}
+              placeholder="Enter title"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <input
+              type="text"
+              id="description"
+              name="description"
+              value={state.description || ""}
+              placeholder="Enter description"
+              onChange={handleInputChange}
+            />
+          </div>
           <div className="upload-section">
             <Dropzone onDrop={onDrop}>
               {({ getRootProps, getInputProps }) => (
@@ -177,17 +166,13 @@ const MedHist = ({ open, onClose }) => {
               </div>
             )}
           </div>
-        </Form>
+          <DialogActions sx={{ mt: "40px" }}>
+            <button type="submit" style={{ backgroundColor: "green", color: "white", width: "30%", marginRight: "20%" }}>
+              Add
+            </button>
+          </DialogActions>
+        </form>
       </DialogContent>
-      <DialogActions sx={{mt:"40px"}}>
-      <Button variant="primary" type="submit" style={{ width: "30%",marginRight:"20%" }}>
-          Add
-        </Button>
-        <Button onClick={onClose} style={{backgroundColor:"red", width: "30%",marginRight:"30px"}}>
-          Cancel
-        </Button>
-  
-      </DialogActions>
       <Snackbar
         open={snackbarOpen}
         autoHideDuration={3000}
